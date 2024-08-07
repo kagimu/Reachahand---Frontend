864bbe003359448970cd83c42b76832a2b9890ea
chrisapx <mcaplexyc@gmail.com>
2024-08-07 16:21:59 +0300

    Applied home page code splitting and section responsiveness

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,224 +1,34 @@
 import React from "react";
 import { animate, motion } from "framer-motion";
-import ImpactCard from "../components/ImpactCard";
 import { impacts, programs } from "../constants";
 import round from "../assets/round.png";
 import ProgramCard from "../components/ProgramCard";
 import Footer from "../components/Footer";
 import NavBar from "../components/NavBar";
-import home1 from "../assets/Web 1920 – 180.png";
-import home2 from "../assets/Web 1920 – 23.jpg";
-import home3 from "../assets/Web 1920 – 24.jpg";
-import home4 from "../assets/Artboard – 2.jpg";
+
 import home5 from "../assets/Web 1920 – 25.jpg";
-import home6 from "../assets/Artboard – 3.jpg";
 import happening from "../assets/happening-1.jpg";
 import arrow from "../assets/icons/featherIcons/arrow-up-right.svg";
+import Section1 from "../components/home/Section1";
+import Section2 from "../components/home/Section2";
+import Section3 from "../components/home/Section3";
 
 const Home = () => {
   return (
-    <div className="w-[100%] content-center max-w-hidden overflow-hidden">
-      <div className="mb-40">
+    <div className="w-full grid grid-cols-1">
+      <div className="mb-40 w-full">
         <NavBar />
       </div>
-      <div className="w-[100%] top-[20%] lg:mx-0">
-        <div className=" mx-10 lg:container">
-          <div className="flex flex-col w-[100%] text-left md:flex md:flex-col md:w-[100%] xl:h-[820px] xl:shrink-0 xl:mb-20 xl:grid xl:grid-rows-3 xl:grid-flow-col xl:gap-4 ">
-            <motion.div
-              initial={{ x: -100, opacity: 0 }}
-              animate={{ x: 0, opacity: 1 }}
-              transition={{
-                delay: 0.2,
-                x: { type: "spring", stiffness: 60 },
-                opacity: { duration: 1 },
-                ease: "easein",
-                duration: 1,
-              }}
-              className="bg-[#0e2d5c] rounded-[30px] w-[100%] h-[100%] relative lg:row-span-3 lg:cols-span-2"
-            >
-              <img
-                src={home1}
-                alt="artboard"
-                className="w-full h-full rounded-[30px] object-cover"
-              />
-              <p className="poppinsRegular text-white -mt-[55%] text-md md:body-1 absolute overflow-hidden lg:-mt-[50%] mx-14">
-                We recognize that young people's concern can only be addressed
-                by young people
-              </p>
-              <h1 className="poppinsBold text-white h1 -mt-[42%] ml-14 text-left leading-tight absolute lg:-mt-[40%] ">
-                <span>We empower young</span> <br />
-                <span>people in Uganda</span>
-              </h1>
-              <a href="GetInvolved">
-                <button className="bg-white lg:-mt-[14%] body-1 -mt-[14%] text-darkBlue ml-14 py-2 px-6 md:py-3 md:px-8 rounded-[25px] absolute overflow-hidden poppinsSemiBold hover:bg-black hover:text-white hover:border-2 hover:border-white">
-                  Get involved
-                </button>
-              </a>
-            </motion.div>
-            <motion.div
-              initial={{ x: 100, opacity: 0 }}
-              animate={{ x: 0, opacity: 1 }}
-              transition={{
-                delay: 0.2,
-                x: { type: "spring", stiffness: 60 },
-                opacity: { duration: 1 },
-                ease: "easein",
-                duration: 1,
-              }}
-              className="flex flex-col lg:grid lg:grid-flow-row lg:gap-4 mt-5 lg:mt-0 mb-10"
-            >
-              <div className="bg-[#0064e6] h-[300px] lg:h-[400px] rounded-[30px] items-baseline lg:mb-0">
-                <img
-                  src={home2}
-                  alt="artboard"
-                  className="w-[100%] h-[300px] mt-5 lg:mt-0 lg:h-[400px] rounded-[30px] object-contain xl:object-cover relative"
-                />
-                <div className="w-[100%] pb-10 absolute overflow-hidden -mt-[50%] lg:-mt-[17%]">
-                  <p className="poppinsSemiBold text-white md:mt-[60px] mb-5 h3 px-10">
-                    We inspire
-                  </p>
-                  <p className="poppinsRegular text-white body-1 text-left px-10">
-                    RAHU inspires youth by introducing them
-                    <br /> to diverse role models and mentors in fields
-                    <br /> like medicine, education, theatre and creative
-                    <br /> arts entrepreneurship
-                  </p>
-                </div>
-              </div>
-              <motion.div
-                initial={{ x: 100, opacity: 0 }}
-                animate={{ x: 0, opacity: 1 }}
-                transition={{
-                  delay: 0.4,
-                  x: { type: "spring", stiffness: 60 },
-                  opacity: { duration: 1 },
-                  ease: "easein",
-                  duration: 1,
-                }}
-                className="grid grid-cols-2 gap-4 h-[360px] lg:h-[410px] rounded-[30px] mt-5 lg:mt-0"
-              >
-                <div className="bg-[#0a9ede] md:h-[360px] lg:h-[410px] rounded-[30px] text-justify">
-                  <div className="px-5 lg:px-10 mt-6">
-                    <div className="flex justify-start items-left">
-                      <p className="poppinsRegular text-[90px] md:text-[100px] text-white">
-                        20
-                      </p>
-                      <div className="flex flex-col mt-4">
-                        <p className="poppinsRegular text-[40px] md:text-[50px] text-white">
-                          +
-                        </p>
-                        <p className="poppinsRegular body-2 text-white mt-16 md:mt-20 mr-20 absolute">
-                          programs
-                        </p>
-                      </div>
-                    </div>
-                    <p className="poppinsRegular text-sm md:body-1 text-left text-white">
-                      Each of RAHU's programs, campaigns or project is tailored
-                      to impact the youth.
-                    </p>
-                    <a href="Programs">
-                      <motion.button
-                        whileHover={{ scale: "1.05" }}
-                        whileTap={{ scale: "-0.95", rotate: "2.5deg" }}
-                        transition={{ duration: "0.125", ease: "easeInOut" }}
-                        className=" mt-5 border-2 border-white body-1 text-white hover:text-white hover:bg-black hover:border-2 hover:border-white py-2 px-6 lg:py-3 lg:px-8 rounded-[25px] poppinsSemiBold"
-                      >
-                        Our Programs
-                      </motion.button>
-                    </a>
-                  </div>
-                </div>
-                <div className=" relative rounded-[30px] h-[360px] lg:h-[410px]">
-                  <img
-                    src={home3}
-                    alt="home"
-                    className="absolute overflow-hidden rounded-[30px] h-[360px] lg:h-[410px] w-[100%] object-cover"
-                  />
-                  <p className="w-2/3 text-left body-1 poppinsSemiBold px-10 mt-[93%] md:mt-80 absolute overflow-hidden ">
-                    Our Events
-                  </p>
-                </div>
-              </motion.div>
-            </motion.div>
-          </div>
+      {/* <div className="bg-red-500 laptop-sm:bg-blue-500 bg-red-500 ">
+        Responsive Div
+      </div> */}
 
-          <motion.div
-            initial={{ x: -100, opacity: 0 }}
-            animate={{ x: 0, opacity: 1 }}
-            transition={{
-              delay: 0.1,
-              x: { type: "spring", stiffness: 60 },
-              opacity: { duration: 0.2 },
-              ease: "easein",
-              duration: 1,
-            }}
-            className="w-full md:h-[680px] lg:h-[700px] mt-10 lg:mt-20 rounded-[25px]"
-          >
-            <img
-              src={home4}
-              alt="impact"
-              className="w-[100%] relative lg:object-cover overflow-x-hidden shrink-0 text-center lg:h-[900px] h-[400px] rounded-[30px]"
-            />
+      <div className="w-full lg:mx-0">
+        <div className="mx-10 ">
+          <Section1/>
+          <Section2/>
 
-            <div className="flex flex-col w-[100%] absolute -mt-[42%] p-5 lg:p-0 lg:mt-0 lg:flex lg:flex-1 lg:flex-row lg:text-left">
-              <div className="w-[100%] justify-left absolute -mt-[10%] lg:-mt-[23%]">
-                <motion.h1
-                  initial={{ x: 100, opacity: 0 }}
-                  whileInView={{ x: 0, opacity: 1 }}
-                  transition={{
-                    delay: 0.4,
-                    x: { type: "spring", stiffness: 60 },
-                    opacity: { duration: 1 },
-                    ease: "easein",
-                    duration: 1,
-                  }}
-                  className="poppinsBold text-lg md:h1 text-white top-2 lg:top-0 ml-20 lg:pt-20 lg:ml-20 -mt-[20%] absolute"
-                >
-                  Our impact
-                </motion.h1>
-                <motion.p
-                  initial={{ x: 100, opacity: 0 }}
-                  animate={{ x: 0, opacity: 1 }}
-                  transition={{
-                    delay: 0.5,
-                    x: { type: "spring", stiffness: 60 },
-                    opacity: { duration: 1 },
-                    ease: "easein",
-                    duration: 1,
-                  }}
-                  className="lg:flex poppinsRegular text-white leading-normal text-left text-sm max-w-[60%] md:body-1 mr-40 ml-4 lg:mr-[40%] -mt-20 lg:h5 lg:ml-20 lg:-mt-60 absolute"
-                >
-                  RAHU inspires youth by introducing them to diverse role models
-                  and mentors in fields like medicine, education, theatre and
-                  creative arts entrepreneurship
-                </motion.p>
-                <div className="mt-2 grid grid-cols-2 gap-2 ml-20 w-[65%] md:gap-4 lg:gap-2 xl:gap-2 xl:w-[70%] md:ml-[15%] md:mt-10 lg:ml-40 lg:-mt-[8%] absolute">
-                  {impacts.map((impact) => (
-                    <ImpactCard
-                      key={impact.label}
-                      total={impact.total}
-                      label={impact.label}
-                    />
-                  ))}
-                </div>
-                <motion.button
-                  initial={{ x: 100, opacity: 0 }}
-                  whileInView={{ x: 0, opacity: 1 }}
-                  transition={{
-                    delay: 0.3,
-                    x: { type: "spring", stiffness: 60 },
-                    opacity: { duration: 1 },
-                    ease: "easein",
-                    duration: 1,
-                  }}
-                  className="py-1 px-6 text-sm xl:py-3 xl:px-8 mt-[40%] md:mt-[50%] lg:mt-[12%] border-2 ml-[5%] md:ml-[10%] lg:ml-40 body-1 border-white absolute overflow-hidden rounded-[25px] poppinsSemiBold text-white"
-                >
-                  Learn more
-                </motion.button>
-              </div>
-            </div>
-          </motion.div>
-
+          
           <motion.div
             initial={{ opacity: 0 }}
             whileInView={{ opacity: 1 }}
@@ -371,13 +181,13 @@ const Home = () => {
 
           <div className="-mt-10 lg:mt-60 text-center justify-center items-center w-[100%]">
             <motion.h1
-              variants={{
-                hidden: { opacity: 0, y: 75 },
-                visible: { opacity: 1, y: 0 },
-              }}
-              initial="hidden"
-              whileInView="visible"
-              transition={{ duration: 0.5, delay: 0.25 }}
+              // variants={{
+              //   hidden: { opacity: 0, y: 75 },
+              //   visible: { opacity: 1, y: 0 },
+              // }}
+              // initial="hidden"
+              // whileInView="visible"
+              // transition={{ duration: 0.5, delay: 0.25 }}
               className="h1 text-black poppinsBold"
             >
               Featured programs
@@ -395,9 +205,9 @@ const Home = () => {
             </div>
             <a href="/Programs">
               <motion.button
-                whileHover={{ scale: "1.00" }}
-                whileTap={{ scale: "-0.95", rotate: "2.5deg" }}
-                transition={{ duration: "0.125", ease: "easeInOut" }}
+                // whileHover={{ scale: "1.00" }}
+                // whileTap={{ scale: "-0.95", rotate: "2.5deg" }}
+                // transition={{ duration: "0.125", ease: "easeInOut" }}
                 className="poppinsRegular px-2 py-2 lg:px-4 lg:py-4 justify-center border-2 border-color-darkBlue mt-10 lg:mt-20 rounded-[30px] body-1 hover:bg-color-darkBlue hover:text-white hover:border-color-lightBlue hover:border-4"
               >
                 See all programs
@@ -405,54 +215,11 @@ const Home = () => {
             </a>
           </div>
 
-          <div className="flex flex-cols-3 gap-2 w-[100%] justify-center mt-40 lg:mt-[6%] mb-[8%] rounded-[30px] lg:p-10">
-            <motion.img
-              variants={{
-                hidden: { opacity: 0, y: 75 },
-                visible: { opacity: 1, y: 0 },
-              }}
-              initial="hidden"
-              whileInView="visible"
-              transition={{ duration: 0.5, delay: 0.25 }}
-              src={home6}
-              alt="HOME6"
-              className="object-cover w-[100%] relative lg:h-full h-[300px] rounded-[30px]"
-            />
-            <motion.div
-              variants={{
-                hidden: { opacity: 0, y: 75 },
-                visible: { opacity: 1, y: 0 },
-              }}
-              initial="hidden"
-              whileInView="visible"
-              transition={{ duration: 0.5, delay: 0.25 }}
-              className="lg:w-[60%] content-center text-center justify-center items-center align-middle absolute overflow-hidden mt-[8%]"
-            >
-              <h1 className=" h5 xl:h1 text-black leading-normal poppinsSemiBold text-center lg:mt-10">
-                We can't do this alone
-              </h1>
-              <h3 className=" body-1 px-40 xl:h5 text-black transition-colors leading-normal poppinsRegular text-center xl:ml-10">
-                At reach a hand we believe in the power of collaboration, you
-                partner <br /> with some of our programs or you can volunteer
-              </h3>
-
-              <a href="GetInvolved">
-                <motion.button
-                  whileHover={{ scale: "1.00" }}
-                  whileTap={{ scale: "0.95", rotate: "2.5deg" }}
-                  transition={{ duration: "0.125", ease: "easeInOut" }}
-                  className="poppinsSemiBold px-6 py-2 lg:px-10 lg:py-4 bg-black text-white mt-10 mb-10 rounded-[30px] body-1 justify-center hover:bg-white hover:text-color-darkBlue hover:border-color-darkMaroon hover:border-2"
-                >
-                  Get Involved
-                </motion.button>
-              </a>
-            </motion.div>
-          </div>
+            <Section3/>
         </div>
-
-        <div className="mt-10 xl:px-20">
-          <Footer />
-        </div>
+      </div>
+      <div className="">
+        <Footer />
       </div>
     </div>
   );
