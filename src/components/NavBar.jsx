ce10d750f2873ed6bdb0c646dd6a558efea58ee4
Dallington <apps@pivosoft.com>
2024-08-10 13:50:37 +0300

    refactored nav bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -61,10 +61,10 @@ const NavBar = () => {
         <nav
           className={`${
             openNavigation ? "flex" : "hidden"
-          } flex flex-row top-[150px] py-10 px-10 justify-between items-center w-full fixed bg-black rounded-xl bg-opacity-75 text-lg poppinsSemiBold lg:static lg:flex lg:mx-auto lg:bg-transparent`}
+          } flex flex-row top-[15px] py-10 px-4 justify-between items-center w-full fixed bg-black rounded-xl bg-opacity-75 text-lg poppinsSemiBold lg:static lg:flex lg:mx-auto lg:bg-transparent`}
         >
           <div
-            className={`flex flex-col justify-center gap-12 top-5 relative z-2 poppinsRegular m-auto ${
+            className={`flex flex-col justify-center gap-8 top-5  items-center justify-center  poppinsRegular m-auto ${
               openNavigation ? "text-white" : "text-[#fff]"
             } lg:flex-row`}
           >
@@ -75,24 +75,24 @@ const NavBar = () => {
                 onMouseLeave={handleMouseLeave}
                 className="relative"
               >
-                <motion.a
+                 <motion.a
                   href={item.href}
                   onClick={handleClick}
-                  className={`block relative transition-colors text-16px md:text-[16px] text-black lg:text-[18px] xl:text-[20px] hover:text-lightBlue${
+                  className={`block relative transition-colors text-base md:text-base text-black lg:text-base xl:text-lg hover:text-lightBlue${
                     location.pathname === `/${item.href}`
-                      ? " poppinsSemiBold text-lightBlue z-5 border-2 border-lightBlue -mt-1 p-2 rounded-[20px]"
+                      ? " poppinsSemiBold text-lightBlue border-2 border-lightBlue -mt-0 p-0 rounded-[10px]"
                       : ""
                   }`}
                 >
                   {item.label}
                 </motion.a>
                 {item.subLinks.length > 0 && activeLink === item.id && (
-                  <div className="absolute max-w-xl left-20 lg:left-0 top-full mt-0 bg-color-lightBlue shadow-xl rounded-md">
+                  <div className="absolute max-w-xl left-2 lg:left-0 top-full mt-0 bg-color-lightBlue shadow-xl rounded-md">
                     {item.subLinks.map((subLink, subIndex) => (
                       <a
                         key={subIndex}
                         href={subLink.href}
-                        className="block w-[250px] px-6 py-2 text-white poppinsSemiBold md:text-[18px] hover:bg-gray-200 hover:text-black"
+                        className="block w-full px-6 py-2 text-white poppinsSemiBold md:text-[18px] hover:bg-gray-200 hover:text-black"
                         onClick={handleClick}
                       >
                         {subLink.label}
@@ -104,9 +104,9 @@ const NavBar = () => {
             ))}
           </div>
 
-          <div className="hidden xl:flex mt-7 -mr-[7%] xl:mr-2">
+          <div className="hidden xl:flex xl:mr-2">
             <a href="PeerFeed">
-              <motion.button className="bg-white px-6 py-4 text-[19px] text-[#343434] rounded-[30px] poppinsRegular hover:bg-[#00274e] hover:text-white hover:border-gold">
+              <motion.button className="bg-white px-4 py-3 text-[19px] text-[#343434] rounded-[30px] poppinsRegular hover:bg-[#00274e] hover:text-white hover:border-gold">
                 Peer academy
               </motion.button>
             </a>
